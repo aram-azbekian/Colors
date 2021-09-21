e5bd96fef964e5420bfdde0e347e0672a51967d8
Aram Azbekian <aram.azbekyan@yandex.com>
2021-09-21 14:54:55 +0300

    Initial Commit

---
+++ Colors/ColorsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorsApp.swift
+//  Colors
+//
+//  Created by Арам on 20.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct ColorsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Colors/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Colors
+//
+//  Created by Арам on 20.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
