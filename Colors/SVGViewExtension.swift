6f7efbbcd15c729ed359ecaf038bea74d38bbac8
Aram Azbekian <aram.azbekyan@yandex.com>
2021-10-04 15:51:08 +0300

    de-integrated Cocoapods

--- Colors/SVGViewExtension.swift
+++ Colors/SVGViewExtension.swift
@@ -9,18 +9,15 @@ import UIKit
 import Macaw
 
 extension SVGView {
-//    func registerForSelection(nodeTag : String) {
-//        self.node.nodeBy(tag: nodeTag)?.onTap { (touch) in
-//            let nodeShape = self.node.nodeBy(tag: nodeTag) as! Shape
-//            nodeShape.fill = Color.blue
-//        }
-//    }
-    
     func registerForSelection() {
         self.node.nodesBy(predicate: { $0.visible && !$0.tag.isEmpty  }).forEach { node in
             node.onTap { event in
                 let nodeShape = node as! Shape
-                nodeShape.fill = Color.blue
+                if nodeShape.fill == Color.white {
+                    nodeShape.fill = Color.blue
+                } else {
+                    nodeShape.fill = Color.white
+                }
             }
         }
     }
